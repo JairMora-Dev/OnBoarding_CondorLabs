9f226e7dbe696076b2ca419daf05ed019ad9c282
Alvaro Agudelo <alvaroagudelo@Admins-iMac.local>
2021-11-12 14:01:39 -0500

    My second commit using MatchBox by CLabs

--- PracCLabs_Acade/MatchBox/src/index.js
+++ PracCLabs_Acade/MatchBox/src/index.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello for using condor labs rules');
-  //console.log('Hello using matchbox CLbas rules');
 });
 
 app.listen(3000);
